ed9893ec0269ff285ce2bb015f05d0a04dad5a68
Kira Korelskaia <kiraswav@gmail.com>
2025-02-11 21:09:39 +0100

    1 exo

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -10,11 +10,12 @@ import java.util.List;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
+      System.out.println("Hi!");
       AnnotationConfigApplicationContext context = 
             new AnnotationConfigApplicationContext(AppConfig.class);
-
+      System.out.println("Hi1!");
       UserService userService = context.getBean(UserService.class);
-
+      System.out.println("H22");
       userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,6 +1,15 @@
 package hiber.model;
 
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Entity;
 import javax.persistence.*;
+
+/*import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id ;
+import jakarta.persistence.GenerationType ;*/
+
 
 @Entity
 @Table(name = "users")
